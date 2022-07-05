87102c7c5b34f439a27d52b28d2fc07f122a1b0f
Chris Diorio <chris.diorio12@gmail.com>
2022-07-05 07:49:30 -0500

    first commit

---
+++ d20modernwp.js
@@ -0,0 +1,10 @@
+Hooks.on("ready", () => {
+  CONFIG.DND5E.weaponProperties["afire"] = "A-Fire";
+  CONFIG.DND5E.weaponProperties["braced"] = "Braced";
+  CONFIG.DND5E.weaponProperties["burstfire"] = "Burst Fire";
+  CONFIG.DND5E.weaponProperties["covert"] = "Covert";
+  CONFIG.DND5E.weaponProperties["mounted"] = "Mounted";
+  CONFIG.DND5E.weaponProperties["sfire"] = "S-Fire";
+  CONFIG.DND5E.weaponProperties["spray"] = "Spray";
+  CONFIG.DND5E.weaponProperties["spread"] = "Spread";
+});
